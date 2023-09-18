09f71da65a51dabfa91f938fc0ecbaddd71ddcce
Tarikul Islam Nahidul <tariqul.islam@ibcs-primax.com>
2023-09-18 11:37:40 +0600

    file stracture update

---
+++ src/Directives/v-tooltip.js
@@ -0,0 +1,25 @@
+// v-tooltip.js
+
+import Vue from 'vue';
+
+// Register a global custom directive called `v-custom-directive`
+Vue.directive('tooltip', {
+  // Bind hook (optional)
+  bind(el, binding) {
+    const tooltip = document.createElement("span");
+    tooltip.className = "tooltip";
+    tooltip.textContent = binding.value;
+    tooltip.style.display = "none";
+
+    el.appendChild(tooltip);
+
+    el.addEventListener("mouseenter", () => {
+      tooltip.style.position = "absolute";
+    });
+
+    el.addEventListener("mouseleave", () => {
+      tooltip.style.display = "none";
+    });
+  },
+
+});
