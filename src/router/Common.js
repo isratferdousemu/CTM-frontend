9baa955f68504e6015c73c2c96a6fd024b0b4c74
Tarikul Islam Nahidul <tariqul.islam@ibcs-primax.com>
2023-09-21 14:33:32 +0600

    igonre file update

--- src/router/Common.js
+++ src/router/Common.js
@@ -2,12 +2,11 @@
 import Login from "../pages/Common/Login/index.vue";
 
 const CommonRoutes = [
-      {
-        path: '/login',
-        name: 'Login',
-        component: Login,
-      }, 
-    ];
-    
-    export default CommonRoutes;
-    
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+];
+
+export default CommonRoutes;
