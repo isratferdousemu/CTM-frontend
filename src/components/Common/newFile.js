1764d15fbd816944c0b12ba7cfb1f82d1f361dc5
Tarikul Islam Nahidul <tariqul.islam@ibcs-primax.com>
2023-09-24 11:19:24 +0600

    crud design update

---
+++ src/components/Common/newFile.js
@@ -0,0 +1,69 @@
+export default (await import('vue')).defineComponent({
+components: {
+NavGroup, NavItem, SubMenu
+},
+data() {
+return {
+menu: false,
+clipped: false,
+fixed: false,
+sidebarMenu: [
+{
+icon: "mdi-view-dashboard-outline",
+title: "Dashboard",
+to: "/",
+permission: "view-dashboard",
+},
+{
+icon: "mdi mdi-cogs",
+title: "System Configuration",
+subItems: [
+{
+title: "Demographic Information",
+icon: "mdi mdi-plus",
+subItems: [
+{
+title: "Division",
+icon: "mdi mdi-plus",
+to: "/system-configuration/division",
+},
+],
+},
+],
+permission: "",
+},
+{
+icon: "mdi-cog-outline",
+title: "Settings",
+to: "/setting/company-setting",
+permission: "view-customers",
+},
+{
+icon: "mdi-chart-box-outline",
+title: "Reports",
+to: "/activity-log",
+permission: "view-customers",
+},
+{
+icon: "mdi-fence",
+title: "Activity Log",
+to: "/activity-log",
+permission: "view-customers",
+},
+],
+miniVariant: false,
+right: true,
+rightDrawer: false,
+};
+},
+computed: {
+Drawer: {
+get() {
+return this.$store.state.Drawer;
+},
+set(v) {
+return this.$store.commit("setDrawer", v);
+},
+},
+},
+});
