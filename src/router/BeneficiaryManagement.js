b250359199ed60f71a9d56ce5af9d6db5546f54a
JunedAhmed <junedahmed848@gmail.com>
2023-10-17 10:46:09 +0600

    Updated

--- src/router/BeneficiaryManagement.js
+++ src/router/BeneficiaryManagement.js
@@ -35,15 +35,6 @@ const BeneficiaryManagementRoutes = [
             "../pages/BeneficiaryManagement/CommitteeManagement/Edit.vue"
           ),
       },
-      {
-        path: "/beneficiary-management/switch_program",
-        name: "beneficiary_list",
-        meta: {
-            requiresAuth: true,
-            permission: "beneficiary-list",
-        },
-        component: () => import("../pages/BeneficiaryManagement/switch_program/Index.vue")
-    },
     {
         path: "/beneficiary-management/beneficiary-card",
         name: "beneficiary_list",
