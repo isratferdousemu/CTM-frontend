150bf8d1399cc4c4ee1c98475bb23a0aeb958712
Tarikul Islam Nahidul <tariqul.islam@ibcs-primax.com>
2023-10-17 10:45:09 +0600

    Merge branch 'development' of https://gitlab.ibcs-primax.com/ctm/frontend into development
    Signed-off-by: Tarikul Islam Nahidul <tariqul.islam@ibcs-primax.com>

--- src/pages/SystemConfiguration/allowance_program/Create.vue
+++ src/pages/SystemConfiguration/allowance_program/Create.vue
@@ -140,7 +140,7 @@ export default {
             this.is_amount = false;
             this.divs = [];
             this.$refs.form.reset();
-            this.$router.push('/system-configuration/allowance-program')
+            this.$router.push('/system-configuration/allowance')
           }
 
           if (this.error_status === 422)
