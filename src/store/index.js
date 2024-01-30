e8139184b2d64b89afc23b53b16f9780df9d6a8c
Sirajul Islam <hasannbd@gmail.com>
2024-01-30 23:30:56 +0600

    lookup type added

--- src/store/index.js
+++ src/store/index.js
@@ -87,7 +87,7 @@ export default new Vuex.Store({
       { id: 18, name: 'Organization' },
       { id: 19, name: 'Designation' },
       { id: 20, name: 'Class' },
-      { id: 21, name: 'Beneficiary Exit Reason' },
+      { id: 21, name: 'Beneficiary Replace Reason' },
       { id: 22, name: 'Beneficiary Shifting Reason' },
     ],
     appLanguage: localStorage.getItem("appLanguage") || process.env.VUE_APP_I18N_LOCALE || 'bn'
