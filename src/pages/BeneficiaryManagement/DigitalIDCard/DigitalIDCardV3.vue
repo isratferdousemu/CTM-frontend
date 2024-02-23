a8a948560ee8a808454ae74ff2b37565f704c282
Sirajul Islam <hasannbd@gmail.com>
2024-02-23 11:05:23 +0600

    ID card issue fix

--- src/pages/BeneficiaryManagement/DigitalIDCard/DigitalIDCardV3.vue
+++ src/pages/BeneficiaryManagement/DigitalIDCard/DigitalIDCardV3.vue
@@ -1,88 +1,93 @@
 <template>
   <div id="digital_id">
     <div class="d-flex justify-center my-5">
-      
+
       <div class="ic-content" id="printIDCard">
-    <div class="ic-header">
-      <div class="ic-header-logo clearfix">
-        <div class="ic-header-logo-left">
-          <img src="/assets/images/logo.png" alt="" class="ic-logo-left"/>
-        </div>
-        <div class="ic-header-logo-right">
-          <img src="/assets/images/bangladesh-govt-logo.png" alt="" class="ic-logo-right"/>
-        </div>
-      </div>
-      <div class="ic-header-avatar">
-        <img v-if="beneficiary.image" :src="beneficiary.image" alt="" class="ic-avatar"/>
-        <img v-if="!beneficiary.image" src="/assets/images/blank-profile-picture-300x300.png" alt="" class="ic-avatar"/>
-      </div>
-    </div>
-    <div class="ic-body">
-      <table>
-              <tr>
-                <th class="text-left" width="50%">
-                  {{
-                    $t(
-                      "container.beneficiary_management.beneficiary_list.beneficiary_name"
-                    )
-                  }}
-                </th>
-                <td>: {{ beneficiary.name_en }}</td>
-              </tr>
-              <tr>
-                <th class="text-left">
-                  {{
-                    $t(
-                      "container.beneficiary_management.beneficiary_list.beneficiary_id"
-                    )
-                  }}
-                </th>
-                <td>: {{ beneficiary.application_id }}</td>
-              </tr>
-              <tr>
-                <th class="text-left">
-                  {{
-                    $t(
-                      "container.beneficiary_management.beneficiary_list.beneficiary_date_of_birth"
-                    )
-                  }}
-                </th>
-                <td>: {{ beneficiary.date_of_birth }}</td>
-              </tr>
-              <tr>
-                <th class="text-left">
-                  {{
-                    $t(
-                      "container.beneficiary_management.beneficiary_list.beneficiary_addess"
-                    )
-                  }}
-                </th>
-                <td>: {{ beneficiary.current_address }}</td>
-              </tr>
-            </table>
-      <table>
-        <tr>
-          <td width="40%">
-            <qr-code v-if="this.beneficiary.qrCode" :text="this.beneficiary.qrCode" size="100"></qr-code>
-            <img v-if="!this.beneficiary.qrCode" src="/assets/images/qr_code.PNG" alt="" class="ic-qrcode"/>
-          </td>
-          <td>
-            <div class="ic-body-signature">
-              <img src="/assets/images/signature.png" alt="" class="ic-signature"/>
+        <div class="ic-header">
+          <div class="ic-header-logo clearfix">
+            <div class="ic-header-logo-left">
+              <img src="/assets/images/logo.png" alt="" class="ic-logo-left" />
             </div>
-            <div class="ic-body-signatory">
-              {{
+            <div class="ic-header-logo-right">
+              <img src="/assets/images/bangladesh-govt-logo.png" alt="" class="ic-logo-right" />
+            </div>
+          </div>
+          <div class="ic-header-avatar">
+            <img v-if="beneficiary.image" :src="beneficiary.image" alt="" class="ic-avatar" />
+            <img v-if="!beneficiary.image" src="/assets/images/blank-profile-picture-300x300.png" alt=""
+              class="ic-avatar" />
+          </div>
+        </div>
+        <div class="ic-body">
+          <table>
+            <tr>
+              <th class="text-left" width="50%">
+                {{
                   $t(
-                    "container.beneficiary_management.beneficiary_list.authorize_singneture"
+                    "container.beneficiary_management.beneficiary_list.beneficiary_name"
                   )
                 }}
-            </div>
-          </td>
-        </tr>
-      </table>
-    </div>
-    <div class="ic-footer">&nbsp;</div>
-  </div>
+              </th>
+              <td>: {{ beneficiary.name_en }}</td>
+            </tr>
+            <tr>
+              <th class="text-left">
+                {{
+                  $t(
+                    "container.beneficiary_management.beneficiary_list.beneficiary_id"
+                  )
+                }}
+              </th>
+              <td>: {{ beneficiary.application_id }}</td>
+            </tr>
+            <tr>
+              <th class="text-left">
+                {{
+                  $t(
+                    "container.beneficiary_management.beneficiary_list.beneficiary_date_of_birth"
+                  )
+                }}
+              </th>
+              <td>: {{ beneficiary.date_of_birth }}</td>
+            </tr>
+            <tr>
+              <th class="text-left">
+                {{
+                  $t(
+                    "container.beneficiary_management.beneficiary_list.beneficiary_addess"
+                  )
+                }}
+              </th>
+              <td>: {{ beneficiary.current_address }}</td>
+            </tr>
+          </table>
+          <div class="ic-qrcode-container">
+            <qr-code v-if="this.beneficiary.qrCode" :text="this.beneficiary.qrCode" size="100"></qr-code>
+            <img v-if="!this.beneficiary.qrCode" src="/assets/images/qr_code.PNG" alt="" class="ic-qrcode" />
+          </div>
+          <!-- <table>
+            <tr>
+              <td width="40%">
+                <qr-code v-if="this.beneficiary.qrCode" :text="this.beneficiary.qrCode" size="100"></qr-code>
+                <img v-if="!this.beneficiary.qrCode" src="/assets/images/qr_code.PNG" alt="" class="ic-qrcode" />
+              </td>
+              <td>
+                <div class="ic-body-signature">
+                  <img src="/assets/images/signature.png" alt="" class="ic-signature" />
+                </div>
+                <div class="ic-body-signatory">
+                  {{
+                    $t(
+                      "container.beneficiary_management.beneficiary_list.authorize_singneture"
+                    )
+                  }}
+                </div>
+              </td>
+            </tr>
+          </table> -->
+        </div>
+        <div class="ic-footer">&nbsp;</div>
+      </div>
 
     </div>
     <div class="d-flex justify-center mb-10">
@@ -149,16 +154,18 @@ export default {
     },
     printIDCard() {
       var content = document.getElementById("printIDCard");
-      var printWindow = window.open('', '_blank','left=0,top=0');
+      var printWindow = window.open('', '_blank', 'left=0,top=0');
       printWindow.document.write('<html><head><title>Print - Beneficiary ID Card</title>');
       printWindow.document.write('<link rel="stylesheet" href="/assets/css/style.css" type="text/css"/>');
       printWindow.document.write('</head><body>');
       printWindow.document.write(content.outerHTML);
       printWindow.document.write('</body></html>');
+      printWindow.document.close();
+      printWindow.onafterprint = printWindow.close;
       setTimeout(function () {
         printWindow.print();
-        printWindow.close();
-      }, 3000);
+      }, 2000);
+
     },
   },
   watch: {
