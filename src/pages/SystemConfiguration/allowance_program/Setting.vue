8038713621326a845e27ad68f26109f5bdf2a784
tarikool <ijad.tarikool@gmail.com>
2024-02-25 12:12:57 +0600

    Max age validation

--- src/pages/SystemConfiguration/allowance_program/Setting.vue
+++ src/pages/SystemConfiguration/allowance_program/Setting.vue
@@ -91,7 +91,7 @@ export default {
             return [
                 v => !!v || "Maximum value is required",
                 v => /^\d+$/.test(v) || 'Maximum Age must be a number',
-                // v => (v >= 5 && v <= 115) || 'Age must be between 5 and 115',
+                v => (v >= 5 && v <= 115) || 'Age must be between 5 and 115',
                 v => {
                     const invalidValue = this.updateAllowanceAge.some(item => parseInt(v) < parseInt(item.min_age));
                     return invalidValue ? 'Maximum value cannot be less than the minimum value' : true;
