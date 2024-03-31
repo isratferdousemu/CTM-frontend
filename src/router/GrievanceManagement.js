13f872211d541648428111e8a1bacd0a5129f100
Anwar Hosssain <anwarhossain.csl@gmail.com>
2024-03-31 15:24:22 +0600

    grievancce type and subject design

--- src/router/GrievanceManagement.js
+++ src/router/GrievanceManagement.js
@@ -1,9 +1,34 @@
 const GrievanceManagementRoutes = [
-  //   {
-  //     path: '/login',
-  //     name: 'Login',
-  //     component: Login,
-  //   },
+  {
+    path: "/grievance-management/type",
+    name: "GrievanceTypeListView",
+    meta: {
+      requiresAuth: true,
+      Permission: "type-list",
+    },
+    component: () =>
+      import("../pages/GrievanceManagement/GrievanceType/index.vue"),
+  },
+  {
+    path: "grievance-management/settings",
+    name: "GrievanceSettings",
+    meta: {
+      requiresAuth: true,
+      Permission: "grievance-settings",
+    },
+    component: () =>
+      import("../pages/GrievanceManagement/GrievanceSettings/index.vue"),
+  },
+  {
+    path: "/grievance-management/subject",
+    name: "GrievanceTypeSubject",
+    meta: {
+      requiresAuth: true,
+      Permission: "subject-list",
+    },
+    component: () =>
+      import("../pages/GrievanceManagement/GrievaneSubject/index.vue"),
+  },
 ];
 
 export default GrievanceManagementRoutes;
