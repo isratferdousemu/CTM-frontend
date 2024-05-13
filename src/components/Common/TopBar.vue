489ac55d1e66a220cf9d9083f41d4cfe78147d0f
unknown <shohorab.ahmed@ibcs-primax.com>
2024-05-13 15:44:35 +0600

    changes in profile setting

--- src/components/Common/TopBar.vue
+++ src/components/Common/TopBar.vue
@@ -1,31 +1,30 @@
 <template lang="">
   <v-app-bar
     elevation="0"
-     height="70"
+    height="70"
     fixed
     app
     class="mx-5 my-3 rounded-lg header-left menu-top"
     style="box-shadow: 3px 8px 14px -5px rgb(0 0 0 / 14%) !important"
   >
- 
     <!--  <v-app-bar-nav-icon @click.stop="drawer = !drawer" /> -->
     <v-btn icon @click.stop="Drawer = !Drawer">
       <v-icon>mdi-menu</v-icon>
     </v-btn>
     <v-img
-        class="text-center ma-auto mr-2"
-        max-height="100%"
-        max-width="50px"
-        position="center center"
-        src="/assets/images/logo.png"
-      ></v-img>
-         <v-spacer />
+      class="text-center ma-auto mr-2"
+      max-height="100%"
+      max-width="50px"
+      position="center center"
+      src="/assets/images/logo.png"
+    ></v-img>
+    <v-spacer />
     <!-- <b> {{  this.$t("topSidebar.title") }}  <p :style="{ 'margin-left': '15%', 'margin-top': '15%' }">
    </br> <span v-if="userData?.office">{{ userData?.office?.office_address}}</span>
     <span v-else>{{  this.$t("topSidebar.location") }}</span>
     </p>
     </b> -->
-<!-- <v-row no-gutters class="ml-2">
+    <!-- <v-row no-gutters class="ml-2">
   <v-col cols="12" md="12" lg="12" sm="12" xs="12">
     <span :class="$vuetify.breakpoint.xs ? 'text-xs' : ''">{{ this.$t("topSidebar.title") }}</span>
   </v-col>
@@ -35,24 +34,29 @@
   </v-col>
 </v-row> -->
 
-
-  <v-spacer class="hidden-xs-only" />
+    <v-spacer class="hidden-xs-only" />
 
     <v-row no-gutters class="ml-2" v-if="$vuetify.breakpoint.smAndUp">
       <v-col cols="12" md="12" lg="12" sm="12" xs="12">
-        <span :class="$vuetify.breakpoint.xs ? 'text-xs' : ''">{{ this.$t("topSidebar.title") }}</span>
+        <span :class="$vuetify.breakpoint.xs ? 'text-xs' : ''">{{
+          this.$t("topSidebar.title")
+        }}</span>
       </v-col>
       <v-col cols="12" md="12" lg="12" sm="12" xs="12">
-        <span :class="$vuetify.breakpoint.xs ? 'text-xs' : ''" v-if="userData?.office">{{ userData?.office?.office_address }}</span>
-        <span :class="$vuetify.breakpoint.xs ? 'text-xs' : ''" v-else>{{ this.$t("topSidebar.location") }}</span>
+        <span
+          :class="$vuetify.breakpoint.xs ? 'text-xs' : ''"
+          v-if="userData?.office"
+          >{{ userData?.office?.office_address }}</span
+        >
+        <span :class="$vuetify.breakpoint.xs ? 'text-xs' : ''" v-else>{{
+          this.$t("topSidebar.location")
+        }}</span>
       </v-col>
     </v-row>
 
-   <v-spacer class="hidden-sm-and-down" />
+    <v-spacer class="hidden-sm-and-down" />
 
-
-   
-     <LocaleSwitcher />
+    <LocaleSwitcher />
 
     <!-- Notification Start -->
     <v-menu
@@ -62,13 +66,12 @@
       width="350"
     >
       <template v-slot:activator="{ on, attrs }">
-
-        <div style="margin-top: 15px; margin-left: 5px; ">
-          <b>{{userData.full_name}}</b>
-          <p style="font-size: 14px;">{{userData.roleNames.join(', ')}}</p>
+        <div style="margin-top: 15px; margin-left: 5px">
+          <b>{{ userData.full_name }}</b>
+          <p style="font-size: 14px">{{ userData.roleNames.join(", ") }}</p>
         </div>
 
-  <v-btn color="indigo" dark v-bind="attrs" v-on="on" icon>
+        <v-btn color="indigo" dark v-bind="attrs" v-on="on" icon>
           <v-badge
             v-if="notificationUnseen > 0"
             :content="notificationUnseen"
@@ -78,19 +81,25 @@
         </v-btn>
       </template>
 
-<v-card id="notification" style="min-width: 350px">
-  <v-list>
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title class="font-weight-bold subtitle-1">NOTIFICATIONS</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
-  </v-list>
+      <v-card id="notification" style="min-width: 350px">
+        <v-list>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-bold subtitle-1"
+                >NOTIFICATIONS</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
 
-  <v-divider></v-divider>
+        <v-divider></v-divider>
 
-  <v-list three-line class="pa-0" style="overflow-y: scroll; height: 350px">
-    <template v-for="(notification, index) in notification">
+        <v-list
+          three-line
+          class="pa-0"
+          style="overflow-y: scroll; height: 350px"
+        >
+          <template v-for="(notification, index) in notification">
             <div :key="index">
               <v-divider v-if="index != 0"></v-divider>
 
@@ -127,24 +136,30 @@
               </v-list-item>
             </div>
           </template>
-  </v-list>
-  <v-list style="background: #eaeaea; padding: 0; margin: 0">
-    <v-list-item @click="seeMore()">
-      <v-list-item-action>
-        <v-icon>mdi-bell</v-icon>
-      </v-list-item-action>
-      <v-list-item-content>
-        <v-list-item-title>ALL NOTIFICATIONS</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
-  </v-list>
-</v-card>
-</v-menu>
-<!-- Notification End -->
+        </v-list>
+        <v-list style="background: #eaeaea; padding: 0; margin: 0">
+          <v-list-item @click="seeMore()">
+            <v-list-item-action>
+              <v-icon>mdi-bell</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>ALL NOTIFICATIONS</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-menu>
+    <!-- Notification End -->
 
-<!-- User link Start -->
-<v-menu transition="scale-transition" origin="top end 0" :close-on-content-click="false" offset-y :nudge-left="150">
-  <template v-slot:activator="{ on, attrs }">
+    <!-- User link Start -->
+    <v-menu
+      transition="scale-transition"
+      origin="top end 0"
+      :close-on-content-click="false"
+      offset-y
+      :nudge-left="150"
+    >
+      <template v-slot:activator="{ on, attrs }">
         <v-btn
           variant="text"
           class="profileBtn custom-hover-primary"
@@ -165,37 +180,51 @@
               style="height: 100%; width: 100%"
             >
               <v-avatar size="35">
-                <img src="/assets/images/avatar-1.jpg" height="35" alt="user" />
+                <!-- <img src="/assets/images/avatar-1.jpg" height="35" alt="user" /> -->
+                <v-img :src="data.photo_url" height="35" alt="user"></v-img>
               </v-avatar>
             </div>
           </v-avatar>
         </v-btn>
       </template>
-  <v-sheet rounded="md" width="200" elevation="10" class="">
-    <v-list class="py-0" lines="one"> 
-      <v-list-item :to="`/profile-info/${userData.id}`" value="item1" active-color="primary">
-        <v-icon size="20">mdi-account-outline</v-icon>
-        <v-list-item-title class="pl-4 text-body-1">My Profile</v-list-item-title>
-      </v-list-item>
-      <!-- <v-list-item value="item2" active-color="primary" to="/login">
+      <v-sheet rounded="md" width="200" elevation="10" class="">
+        <v-list class="py-0" lines="one">
+          <v-list-item
+            :to="`/profile-info/${userData.id}`"
+            value="item1"
+            active-color="primary"
+          >
+            <v-icon size="20">mdi-account-outline</v-icon>
+            <v-list-item-title class="pl-4 text-body-1"
+              >My Profile</v-list-item-title
+            >
+          </v-list-item>
+          <!-- <v-list-item value="item2" active-color="primary" to="/login">
         <v-icon size="20">mdi-cog-outline</v-icon>
         <v-list-item-title class="pl-4 text-body-1">My Account</v-list-item-title>
       </v-list-item> -->
-    </v-list>
-    <div class="pt-4 pb-4 px-5 text-center">
-      <v-btn link @click="logout()" color="primary" outlined block>Logout</v-btn>
-    </div>
-  </v-sheet>
-</v-menu>
-<!-- User link End -->
-</v-app-bar>
+        </v-list>
+        <div class="pt-4 pb-4 px-5 text-center">
+          <v-btn link @click="logout()" color="primary" outlined block
+            >Logout</v-btn
+          >
+        </div>
+      </v-sheet>
+    </v-menu>
+    <!-- User link End -->
+  </v-app-bar>
 </template>
 
 <script>
 import axios from "axios";
-import LocaleSwitcher from "@/components/Common/LocaleSwitcher"
+import LocaleSwitcher from "@/components/Common/LocaleSwitcher";
 import { mapState } from "vuex";
 export default {
+  data() {
+    return {
+      data: {},
+    };
+  },
 
   components: { LocaleSwitcher },
   computed: {
@@ -205,7 +234,7 @@ export default {
       },
     },
     ...mapState({
-      userData: (state) => state.userData
+      userData: (state) => state.userData,
     }),
     notification: {
       get() {
@@ -230,6 +259,7 @@ export default {
         return this.$store.commit("setDrawer", v);
       },
     },
+    
   },
   methods: {
     logout() {
@@ -242,12 +272,43 @@ export default {
           },
         })
         .then(() => {
-
           this.$store.commit("setToken", null);
           this.$store.commit("setUser", null);
           this.$router.push({ name: "Login" });
         });
     },
+
+    async getUserById() {
+      if (!this.userData || !this.userData.id) {
+      console.log("User data or user ID is not available yet.");
+      return;
+    }
+    
+      try {
+        this.$axios
+          .get(`/admin/user/get-user/${this.userData?.id}`, {
+            headers: {
+              Authorization: "Bearer " + this.$store.state.token,
+              "Content-Type": "multipart/form-data",
+            },
+          })
+          .then((res) => {
+            if (res.data) {
+              this.data = res.data.data;
+            } else {
+              this.$toast.error("Something went wrong");
+            }
+          })
+          .catch((err) => {
+          });
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+
+  mounted() {
+    this.getUserById();
   },
 };
 </script>
