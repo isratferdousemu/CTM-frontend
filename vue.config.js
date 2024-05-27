860ae53f9108d83e22b675c1b9098db2ae213ef5
Md Atikur Rahman <atikur.rahman@ibcs-primax.com>
2024-05-27 08:57:54 +0000

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   devServer: {
     port: process.env.VUE_APP_PORT || 8080,
-    proxy: {
-      '/api': {
-        target: 'https://ctmb.ecommercesheba.com',
-        changeOrigin: true,
-        pathRewrite: { '^/api': '' },
-      },
-    },
   },
   pluginOptions: {
     i18n: {
