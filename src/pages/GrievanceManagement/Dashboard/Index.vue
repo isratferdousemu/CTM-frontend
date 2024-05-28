dd1cf4f2f2857692af3cd8f61fd6c15390d2ca58
Anwar Hosssain <anwarhossain.csl@gmail.com>
2024-05-28 10:46:42 +0600

    env

--- src/pages/GrievanceManagement/Dashboard/Index.vue
+++ src/pages/GrievanceManagement/Dashboard/Index.vue
@@ -1,32 +1,39 @@
 <template>
   <v-container fluid>
-     <v-card-title  tag="div"
-                    style="background-color:#1c3b68;color:white;margin-bottom: 17px;font-size:17px;">
-                    <h3 class="white--text">
-                      {{ $t("container.grievance_management.dashboard.grievance_dashboard") }}
-                    </h3>
-     </v-card-title>
+    <v-card-title tag="div" style="background-color:#1c3b68;color:white;margin-bottom: 17px;font-size:17px;">
+      <h3 class="white--text">
+        {{ $t("container.grievance_management.dashboard.grievance_dashboard") }}
+      </h3>
+    </v-card-title>
 
-     <!-------start  header Card component ---->
+    <!-------start  header Card component ---->
     <v-row align="center" justify="center" dense>
       <v-col cols="12" md="4">
-        <v-card
-          append-icon='mdi-calendar'
-          class="mx-auto"
-          prepend-icon="mdi-account"
-          subtitle="prepend-icon and append-icon"
-          title="Icons"
-        >
-          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
+        <v-card>
+          <v-row>
+            <v-col cols="12" md="3" lg="2" sm="2">
+              <v-card-title>
+                <v-icon large color="green darken-2">
+                  mdi-domain
+                </v-icon>
+              </v-card-title>
+            </v-col>
+            <v-col cols="12" lg="9" md="6" sm="6">
+              <v-card-title>Total Grievance Recived</v-card-title>
+            </v-col>
+          </v-row>
+          <v-row class="justify-content-center">
+            <v-col cols="12" md="12">
+              <v-card-title>
+                12345
+              </v-card-title>
+            </v-col>
+          </v-row>
         </v-card>
       </v-col>
 
       <v-col cols="12" md="4">
-        <v-card
-          class="mx-auto"
-          subtitle="prepend and append"
-          title="Icons"
-        >
+        <v-card class="mx-auto" subtitle="prepend and append" title="Icons">
           <template v-slot:prepend>
             <v-icon color="primary" icon="mdi-account"></v-icon>
           </template>
@@ -38,19 +45,15 @@
       </v-col>
 
       <v-col cols="12" md="4">
-        <v-card
-          append-avatar="https://cdn.vuetifyjs.com/images/john.jpg"
-          class="mx-auto"
-          prepend-avatar="https://cdn.vuetifyjs.com/images/logos/v-alt.svg"
-          subtitle="prepend-avatar and append-avatar"
-          title="Avatars"
-        >
+        <v-card append-avatar="https://cdn.vuetifyjs.com/images/john.jpg" class="mx-auto"
+          prepend-avatar="https://cdn.vuetifyjs.com/images/logos/v-alt.svg" subtitle="prepend-avatar and append-avatar"
+          title="Avatars">
           <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
         </v-card>
       </v-col>
 
     </v-row>
-     <!-------End  header Card component ------->
+    <!-------End  header Card component ------->
 
     <v-row class="mt-3">
 
@@ -58,7 +61,7 @@
         <v-card>
           <v-card-text>
             <V-row>
-                  <TotalNumerReceivedDoughnutChart/>
+              <TotalNumerReceivedDoughnutChart />
             </V-row>
           </v-card-text>
         </v-card>
@@ -67,7 +70,7 @@
         <v-card height="100%">
           <v-card-text>
             <V-row>
-              <TotalNumerApprovePieChart/>
+              <TotalNumerApprovePieChart />
             </V-row>
           </v-card-text>
         </v-card>
@@ -76,7 +79,7 @@
         <v-card height="100%">
           <v-card-text>
             <V-row>
-              <ProgramWiseWaitingPieChart/>
+              <ProgramWiseWaitingPieChart />
             </V-row>
           </v-card-text>
         </v-card>
@@ -85,7 +88,7 @@
         <v-card height="100%">
           <v-card-text>
             <V-row>
-              <NumerofApplicationRejectedDoughnutChart/>
+              <NumerofApplicationRejectedDoughnutChart />
             </V-row>
           </v-card-text>
         </v-card>
@@ -97,7 +100,7 @@
         <v-card height="100%">
           <v-card-text>
             <V-row>
-              <TotalNumerReceivedLineBarChart/>
+              <TotalNumerReceivedLineBarChart />
             </V-row>
           </v-card-text>
         </v-card>
@@ -107,7 +110,7 @@
         <v-card height="100%">
           <v-card-text>
             <V-row>
-              <TotalNumerForwardedBarChart/>
+              <TotalNumerForwardedBarChart />
             </V-row>
           </v-card-text>
         </v-card>
@@ -159,7 +162,7 @@ export default {
   methods: {
 
   },
-  mounted(){
+  mounted() {
     this.drawer = false;
   },
   beforeMount() {
