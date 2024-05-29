8e1ed1051e1af68c974beeafb71e8bd117bea0a2
Rezwan <rezwan.ahmed@ibcs-primax.com>
2024-05-29 10:00:44 +0600

    Emergency allotment menu setup done

--- src/router/EmergencyPaymentManagement.js
+++ src/router/EmergencyPaymentManagement.js
@@ -1,9 +1,22 @@
 const EmergencyPaymentManagementRoutes = [
-  //   {
-  //     path: '/login',
-  //     name: 'Login',
-  //     component: Login,
-  //   },
+  {
+    path: "/emergency-payment/emergency-allotment",
+    name: "emergencyAllotment",
+    meta: {
+      requiresAuth: true,
+      permission: "emergencyAllotment-view",
+    },
+    component: () => import("../pages/EmergencyPaymentManagement/Index.vue"),
+  },
+  {
+    path: "/emergency-payment/emergency-allotment/create",
+    name: "emergencyAllotment",
+    meta: {
+      requiresAuth: true,
+      permission: "emergencyAllotment-create",
+    },
+    component: () => import("../pages/EmergencyPaymentManagement/Create.vue"),
+  },
 ];
 
 export default EmergencyPaymentManagementRoutes;
